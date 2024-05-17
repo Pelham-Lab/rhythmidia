c482a31f88479acda4053805608bc972dd1c6e69
akeeley <akeeley@holehouselabs-MacBook-Air.local>
2024-05-17 15:27:26 -0500

    Attempting to get readthedocs to compile.

--- rhythmidia/__init__.py
+++ rhythmidia/__init__.py
@@ -1,17 +1,17 @@
 """Race tube image analysis and period elucidation in Python."""
 
 # Add imports here
-import importlib
+#import importlib
 disp = True
 try:
     import pyautogui
 except KeyError:
     disp = False
 if disp:
-    importlib.import_module(rhythmidia)
-    importlib.import_module(rhythmidia_gui)
-#from .rhythmidia import *
-#from .rhythmidia_gui import *
+    #importlib.import_module(.rhythmidia)
+    #importlib.import_module(rhythmidia_gui)
+    from .rhythmidia import *
+    from .rhythmidia_gui import *
 
 
 from ._version import __version__
